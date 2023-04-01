2e26aae79942187a9fad22f26819f56f598553c1
Aryan Mathur <aryan27mathur@gmail.com>
2023-04-01 17:32:06 -0400

    works with flask and react, basic demo with string length being added to input

--- Backend/app.py
+++ Backend/app.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
 
 @app.route('/')
 def start():
     return "Hello World!"
 
-@app.route('/imed/get-disease/<input>')
-def get_disease(input):
-    return f'{input}'
+
+@app.route('/imed/get-disease/')
+@cross_origin()
+def get_disease():
+    input_value = request.args.get('input')
+
+    data = {'message': input_value + ' ' + str(len(input_value))}
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from "axios";
 /*
 const App = () => {
@@ -52,46 +52,56 @@ export default App; */
 
 
 const App = () => {
-    const [inputText, setInputText] = useState('');
-    const [classification, setClassification] = useState('');
-  
-    const handleChange = (e) => {
-      setInputText(e.target.value);
-    };
-  
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      const result = mockClassifier(inputText);
-      setClassification(result);
-    };
-  
-    const mockClassifier = (text) => {
-      // Replace this with your own classifier function
-      return text.length % 2 === 0 ? 'Flu' : 'Cancer';
-    };
-  
-    return (
-      <div>
-        <h1>Text Classifier</h1>
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="inputText">Enter your text:</label>
-          <textarea
-            id="inputText"
-            value={inputText}
-            onChange={handleChange}
-            rows={4}
-            cols={50}
-          ></textarea>
-          <button type="submit">Classify</button>
-        </form>
-        {classification && (
-          <div>
-            <h2>Classification Result:</h2>
-            <p>{classification}</p>
-          </div>
-        )}
-      </div>
-    );
-  };
+  const [inputText, setInputText] = useState('');
+  const [classification, setClassification] = useState('');
+
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch(`http://127.0.0.1:5000/imed/get-disease/?input=${inputText}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('lets go');
+        setClassification(data.message);
+      })
+      .catch((error) => {
+        // handle errors here
+      });
+    //const result = mockClassifier(inputText);
+
+  };
+
+  const mockClassifier = (text) => {
+    // Replace this with your own classifier function
+    return text.length % 2 === 0 ? 'Flu' : 'Cancer';
+  };
+
+
+  return (
+    <div>
+      <h1>Text Classifier</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="inputText">Enter your text:</label>
+        <textarea
+          id="inputText"
+          value={inputText}
+          onChange={handleChange}
+          rows={4}
+          cols={50}
+        ></textarea>
+        <button type="submit">Classify</button>
+      </form>
+      {classification && (
+        <div>
+          <h2>Classification Result:</h2>
+          <p>{classification}</p>
+        </div>
+      )}
+    </div>
+  );
+};
   
   export default App;
