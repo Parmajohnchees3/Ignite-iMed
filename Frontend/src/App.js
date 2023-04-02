915ed1e0c2bd54c836ca81493c8256af752cd903
John Yoo <jsy12@duke.edu>
2023-04-02 12:29:38 -0400

    react edited

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,52 +1,90 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-import React, { useState } from "react";
-import axios from "axios";
-
+import React, { useState } from 'react';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Container from '@mui/material/Container';
+import axios from 'axios';
+import './App.css';
 
 const App = () => {
-  const [inputText, setInputText] = useState("");
-  const [classification, setClassification] = useState("");
+  const [inputText, setInputText] = useState('');
+  const [classification, setClassification] = useState('');
 
   const handleChange = (e) => {
     setInputText(e.target.value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    axios
-      .get(`http://127.0.0.1:5000/api/process_data/?input=${inputText}`)
-      .then((response) => {
-        setClassification(response.data.disease);
-      })
-      .catch((error) => {});
+    try {
+      const response = await axios.get(
+        `http://127.0.0.1:5000/api/process_data/?input=${inputText}`
+      );
+      setClassification(response.data.disease);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
   };
 
   return (
-    <div>
-      <h1>iMed: Diagnostic Tool</h1>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="inputText">Enter your symptoms:</label>
-        <textarea
-          id="inputText"
-          value={inputText}
-          onChange={handleChange}
-          rows={4}
-          cols={50}
-        ></textarea>
-        <button type="submit">Submit</button>
-      </form>
-
+    <Container maxWidth="sm">
+      <Box sx={{ mt: 8, mb: 2 }}>
+        <Typography variant="h3" component="h1" align="center">
+          Disease Predictor
+        </Typography>
+      </Box>
+      <Card>
+        <CardContent>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            sx={{
+              '& > :not(style)': { mt: 2, mb: 2 },
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+            }}
+          >
+            <TextField
+              id="inputText"
+              label="Enter your symptoms"
+              multiline
+              rows={4}
+              value={inputText}
+              onChange={handleChange}
+              fullWidth
+              required
+            />
+            <Button type="submit" variant="contained" color="primary">
+              Submit
+            </Button>
+          </Box>
+        </CardContent>
+      </Card>
       {classification && (
-        <div>
-          <h2>Classification Result:</h2>
-          {classification != "No disease found" ?
-          <p>You may have this condition: {classification.toString().toLowerCase()}</p>
-            : <p>We were not able to determine your condition. Please add as many symptoms you are experiencing as possible.</p>}
-        </div>
+        <Card sx={{ mt: 4 }}>
+          <CardContent>
+            <Typography variant="h5" component="h2">
+              Prediction:
+            </Typography>
+            {classification !== 'No disease found' ? (
+              <Typography variant="body1">
+                You may have this condition:{' '}
+                {classification.toString().toLowerCase()}
+              </Typography>
+            ) : (
+              <Typography variant="body1">
+                We were not able to determine your condition. Please add as
+                many symptoms you are experiencing as possible.
+              </Typography>
+            )}
+          </CardContent>
+        </Card>
       )}
-    </div>
+    </Container>
   );
 };
 
